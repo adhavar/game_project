c56489030c8d793ae8b0787d7dfbe32f688636f3
Kezlyn Margareth <kmargareth@my.smccd.edu>
2026-09-24 11:38:39 -0700

    Adding play again loop

--- guessing_game.py
+++ guessing_game.py
@@ -14,34 +14,38 @@ def guessing_game():
 
     Author: Kezlyn Margareth
     """
-    print("Hi There! Welcome to Guessing Game!")
-    print("I'm thingking of a number between 1 to 100.")
-    print("I will give you 5 attemps. Try to guess what the number is.")
-    print("Good luck!")
+    again = 'Y'
+    while again.upper() == 'Y':
+        print("\nHi there! Welcome to Guessing Game!")
+        print("I'm thinking of a number between 1 to 100.")
+        print("I will give you 5 attempts. Try to guess what the number is.")
+        print("Good luck!")
 
-    number = random.randint(1,100)
-    i = 5
+        number = random.randint(1,100)
+        i = 5
 
-    while i > 0:
-        user_number = int(input('\nEnter your guess: '))
-        if user_number < number:
-            print("Too low.", end= " ")
-        elif user_number > number:
-            print("Too high.", end= " ")
-        else:
-            print("You guessed it! Congratulation!")
-            break
+        while i > 0:
+            user_number = int(input('\nEnter your guess: '))
+            if user_number < number:
+                print("Too low.", end= " ")
+            elif user_number > number:
+                print("Too high.", end= " ")
+            else:
+                print("You guessed it! Congratulation!")
+                break
 
-        i -= 1
+            i -= 1
 
-        if i == 1:
-            print("Try again!\nYou only have 1 more attempt")
-        elif i > 0:
-            print("Try again!\nYou have", i, "more attempts")
+            if i == 1:
+                print("Try again!\nYou only have 1 more attempt")
+            elif i > 0:
+                print("Try again!\nYou have", i, "more attempts")
         
 
-    if i == 0:
-        print(f'You lost! The number is {number}')
+        if i == 0:
+            print(f'You lost! The number is {number}')
+            
+        again = input("\nDo you want to play agin (Y/N): ")
 
 if __name__== "__main__":
     guessing_game()
