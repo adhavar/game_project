ba3d1edcf12078a137456622a243656caf781aff
Adhamaryz Vargas <adhamaryz.vargas@gmail.com>
2026-09-24 09:01:47 -0700

    Add main.py

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 # Lab 1
 # Group 2
-# Author: 
-# Date: 
+# Author: Adhamaryz Vargas
+# Date: 09/22/2026
+
+# Import the games
+from guessing_game import guessing_game
+from rps_game import rps_game
+
+# Select the game based on user's input
+choice = int(input("Which game do you want to play? 1. Guessing Game, 2. Rock-paper-scissors. "))
+if choice == 1:
+    guessing_game()
+elif choice == 2:
+    rps_game()
+
+# Assign value to play
+play = input("Do you want to play again? (Y/N) ")
+
+# while loop: runs as long as the user inputs "Y" or "YES"
+while play.upper() == "Y" or play.upper() == "YES":
+    choice = int(input("Which game do you want to play? 1. Guessing Game, 2. Rock-paper-scissors. "))
+    if choice == 1:
+        guessing_game()
+    elif choice == 2:
+        rps_game()
+    play = input("Do you want to play again? (Y/N) ")
+
+# Message if answer is anything but "y" or "yes"
+print("Thank you for playing!")
